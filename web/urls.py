48baa7750b423835514edb3a9f7ccdfcc7dd6d9d
Vittoria <vittoria.piatti@yahoo.it>
2022-11-18 19:59:25 +0200

    UPDATE urls to fit new templates

--- web/urls.py
+++ web/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
+    path('login_page', views.login_page, name='login_page'),
+    path('register_page', views.register_page, name='register_page'),
     path('create_product', views.create_product, name='create_product')
 ]
